df579e5e1c88ba0e4da46f2e948e3bce241dd2d3
Nikita Tribunskiy <nikita.tribunskiy@gmail.com>
2022-09-07 12:07:09 +0300

    Add reference to nuget integration tests project

--- build/Build.cs
+++ build/Build.cs
@@ -35,7 +35,7 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main() => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile, x => x.Pack, x => x.TestPackage);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
